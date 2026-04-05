5736777feb3bab6e1f285ecbd495046cd731f010
Nick Steinmann <nick.steinmann@gmx.de>
2026-04-05 16:03:02 +0200

    Create card element for each animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -15,19 +15,21 @@ output = ""
 
 for animal in animals_data:
 
+    output += '<li class="cards__item">\n'
+
     if "name" in animal:
-        output += f"Name: {animal['name']}\n"
+        output += f"Name: {animal['name']}<br/>\n"
 
     if "characteristics" in animal and "diet" in animal["characteristics"]:
-        output += f"Diet: {animal['characteristics']['diet']}\n"
+        output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
 
     if "locations" in animal and len(animal["locations"]) > 0:
-        output += f"Location: {animal['locations'][0]}\n"
+        output += f"Location: {animal['locations'][0]}<br/>\n"
 
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f"Type: {animal['characteristics']['type']}\n"
+        output += f"Type: {animal['characteristics']['type']}<br/>\n"
 
-    output += "\n"
+    output += '</li>\n'
 
 # Read HTML template
 with open("animals_template.html", "r") as file:
